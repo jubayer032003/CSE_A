8fc4bfcb4ad8dc3ba26bfb7d5784b09522f97f62
Jubayer Rahman Chowdhury <jubayer.at.03@gmail.com>
2026-06-08 23:41:19 +0600

    Fix Express catch-all route for SPA in Express 5

--- backend/server.js
+++ backend/server.js
@@ -84,7 +84,7 @@ io.on("connection", (socket) => {
 // Serve built frontend assets
 app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
 });
 
